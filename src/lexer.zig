9b2267c5eaa01027d99fc58ec80a9fb793a8c961
DrunkTaric <comborush.init@proton.me>
2024-05-26 16:35:10 +0300

    last test

--- src/lexer.zig
+++ src/lexer.zig
@@ -339,3 +339,95 @@ test "test 3rd phase Lexer" {
     }
 }
 
+test "test Final phase Lexer" {
+    const input =
+        \\let five = 5;
+        \\let ten = 10;
+        \\let add = method(x, y) {
+        \\x + y;
+        \\};
+        \\let result = add(five, ten);
+        \\!-/*5;
+        \\5 < 10 > 5;
+        \\when (5 < 10) {
+        \\export true;
+        \\} otherwise {
+        \\export false;
+        \\}
+    ;
+    var lex = Lexer.init(input);
+    const tokens = [_]Token{
+        .LET,
+        .{ .IDENTIFIER = "five" },
+        .ASSIGN,
+        .{ .NUMBER = &.{53} },
+        .SEMICOLON,
+        .LET,
+        .{ .IDENTIFIER = "ten" },
+        .ASSIGN,
+        .{ .NUMBER = &.{ 49, 48 } },
+        .SEMICOLON,
+        .LET,
+        .{ .IDENTIFIER = "add" },
+        .ASSIGN,
+        .FUNCTION,
+        .LPAREN,
+        .{ .IDENTIFIER = "x" },
+        .COMMA,
+        .{ .IDENTIFIER = "y" },
+        .RPAREN,
+        .LBRACE,
+        .{ .IDENTIFIER = "x" },
+        .PLUS,
+        .{ .IDENTIFIER = "y" },
+        .SEMICOLON,
+        .RBRACE,
+        .SEMICOLON,
+        .LET,
+        .{ .IDENTIFIER = "result" },
+        .ASSIGN,
+        .{ .IDENTIFIER = "add" },
+        .LPAREN,
+        .{ .IDENTIFIER = "five" },
+        .COMMA,
+        .{ .IDENTIFIER = "ten" },
+        .RPAREN,
+        .SEMICOLON,
+        .BANG,
+        .MINUS,
+        .DEVIDE,
+        .MULTIPLY,
+        .{ .NUMBER = &.{53} },
+        .SEMICOLON,
+        .{ .NUMBER = &.{53} },
+        .LESSTHAN,
+        .{ .NUMBER = &.{ 49, 48 } },
+        .GREATERTHAN,
+        .{ .NUMBER = &.{53} },
+        .SEMICOLON,
+        .IF,
+        .LPAREN,
+        .{ .NUMBER = &.{53} },
+        .LESSTHAN,
+        .{ .NUMBER = &.{ 49, 48 } },
+        .RPAREN,
+        .LBRACE,
+        .RETURN,
+        .{ .BOOLEAN = true },
+        .SEMICOLON,
+        .RBRACE,
+        .ELSE,
+        .LBRACE,
+        .RETURN,
+        .{ .BOOLEAN = false },
+        .SEMICOLON,
+        .RBRACE,
+    };
+    for (tokens) |token| {
+        const tok = lex.nextToken();
+        //std.debug.print("Current Token From lexer is: {?}\n", .{tok});
+        //std.debug.print("Current Token From array is: {?}\n", .{token});
+        //std.debug.print("=======================\n", .{});
+        try expectEqualDeep(token, tok);
+    }
+}
