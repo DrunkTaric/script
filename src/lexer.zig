9c2d970253fbc69aefbf4d86253df4187a0f5313
DrunkTaric <comborush.init@proton.me>
2024-05-26 16:34:23 +0300

    adding another test

--- src/lexer.zig
+++ src/lexer.zig
@@ -209,3 +209,62 @@ test "test getnextToken()" {
         try expectEqualDeep(token, tok);
     }
 }
+
+test "test 2nd phase in lexer" {
+    const input =
+        \\let five = 5;
+        \\let ten = 10;
+        \\let add = method(x, y) {
+        \\x + y;
+        \\};
+        \\let result = add(five, ten);
+    ;
+    var lex = Lexer.init(input);
+    const tokens = [_]Token{
+        .LET,
+        .{ .IDENTIFIER = "five" },
+        .ASSIGN,
+        .{ .NUMBER = &.{53} },
+        .SEMICOLON,
+        .LET,
+        .{ .IDENTIFIER = "ten" },
+        .ASSIGN,
+        .{ .NUMBER = &.{ 49, 48 } },
+        .SEMICOLON,
+        .LET,
+        .{ .IDENTIFIER = "add" },
+        .ASSIGN,
+        .FUNCTION,
+        .LPAREN,
+        .{ .IDENTIFIER = "x" },
+        .COMMA,
+        .{ .IDENTIFIER = "y" },
+        .RPAREN,
+        .LBRACE,
+        .{ .IDENTIFIER = "x" },
+        .PLUS,
+        .{ .IDENTIFIER = "y" },
+        .SEMICOLON,
+        .RBRACE,
+        .SEMICOLON,
+        .LET,
+        .{ .IDENTIFIER = "result" },
+        .ASSIGN,
+        .{ .IDENTIFIER = "add" },
+        .LPAREN,
+        .{ .IDENTIFIER = "five" },
+        .COMMA,
+        .{ .IDENTIFIER = "ten" },
+        .RPAREN,
+        .SEMICOLON,
+        .EOF,
+    };
+    for (tokens) |token| {
+        const tok = lex.nextToken();
+        //std.debug.print("Current Token From lexer is: {?}\n", .{tok});
+        //std.debug.print("Current Token From array is: {?}\n", .{token});
+        //std.debug.print("=======================\n", .{});
+        try expectEqualDeep(token, tok);
+    }
+}
+
